b5f9e4d7de9ab6424e74c5084335992dcb2f590d
mt <mt@25aa0472-a5d5-0310-b9f0-db4f2a0ab381>
2007-01-24 06:44:31 +0000

    git-svn-id: file:///home/mt/data/subversion/web/websites/localstatic.com@99 25aa0472-a5d5-0310-b9f0-db4f2a0ab381

--- pages/links.php
+++ pages/links.php
@@ -6,6 +6,7 @@
   <div class="column">
    <h4>News</h4>
    <ul>
+    <li><a href="http://reader.google.com/">Google Reader</a></li>
     <li><a href="https://www.bloglines.com/myblogs">Bloglines</a></li>
     <li><a href="http://www.digg.com/">Digg</a></li>
     <li><a href="http://reddit.com/">reddit</a></li>
@@ -19,17 +20,23 @@
     <li><a href="http://www.penny-arcade.com/view.php3">Penny Arcade</a></li>
     <li><a href="http://www.ctrlaltdel-online.com/index.php?t=archives&amp;date=last">Ctrl+Alt+Del</a></li>
    </ul>
+   
+   <h4>Music</h4>
+   <ul>
+    <li><a href="http://www.pandora.com/">Pandora</a></li>
+    <li><a href="http://music.yahoo.com/launchcast/station.asp?u=1424614547">Launch</a></li>
+    <li><a href="http://music.yahoo.com/launchcast/station.asp?u=2009574741">Launch 2</a></li>
+	<li><a href="http://www.last.fm/user/localstatic/">Last.fm</a></li>
+   </ul>
   </div> <!-- column -->
 
   <div class="column">
    <h4>Misc</h4>
    <ul>
-    <li><a href="http://www.pandora.com/">Pandora</a></li>
-    <li><a href="http://music.yahoo.com/launchcast/station.asp?u=1424614547">Launch</a></li>
-    <li><a href="http://music.yahoo.com/launchcast/station.asp?u=2009574741">Launch 2</a></li>
-    <li><a href="http://www.thisamericanlife.com/">This American Life</a></li>
+	<li><a href="http://localstatic.backpackit.com/">Backpack</a></li>
 	<li><a href="http://www.calorie-count.com/">Calorie Count</a></li>
-	<li><a href="http://www.nationstates.net/">NationStates</a></li>
+	<li><a href="http://www.flickr.com/">Flickr</a></li>
+	<li><a href="http://standard.1000ad.net/">1000 AD</a></li>
    </ul>
 
    <h4>Devel Docs</h4>
@@ -46,7 +53,7 @@
   <div class="column">
    <h4>Bookmarks</h4>
    <ul>
-    <li><a href="http://del.icio.us/mterry">del.icio.us</a></li>
+    <li><a href="http://del.icio.us/localstatic">del.icio.us</a></li>
     <li><a href="/~mt/bookmarks/">Old Bookmarks</a></li>
    </ul>
 
@@ -56,6 +63,17 @@
     <li><a href="http://vista.weber.edu/">WSU Vista</a></li>
 	<li><a href="http://www.wsuslcc.com/">WSU@SLCC</a></li>
     <li><a href="http://wsuslcc.com/3550/">CS3550</a></li>
+   </ul>
+  </div> <!-- column -->
+
+  <div class="column">
+   <h4>Social</h4>
+   <ul>
+    <li><a href="http://del.icio.us/localstatic/">del.icio.us</a></li>
+    <li><a href="http://www.last.fm/user/localstatic/">last.fm</a></li>
+    <li><a href="http://www.flickr.com/photos/32359125@N00/">flickr</a></li>
+    <li><a href="http://www.digg.com/users/localstatic">digg</a></li>
+
    </ul>
   </div> <!-- column -->
 
