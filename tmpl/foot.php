4b21eaba2f927c526a40becb6b43b86cd1dda08b
mt <mt@25aa0472-a5d5-0310-b9f0-db4f2a0ab381>
2007-07-06 05:07:29 +0000

    Moved contents of old template index.php into head & foot php files.
    git-svn-id: file:///home/mt/data/subversion/web/websites/localstatic.com@107 25aa0472-a5d5-0310-b9f0-db4f2a0ab381

---
+++ tmpl/foot.php
@@ -0,0 +1,25 @@
+
+</div>
+
+<div id="footer">
+  <div id="meta">
+    <a href="mailto:localstatic@localstatic.com">Morgan Terry</a><br/>
+    <?php
+    echo date('r', filemtime($include_file));
+    ?>
+  </div>
+
+  <div id="icons">
+    <a href="http://validator.w3.org/check/referer">
+      <img style="border:0;width:88px;height:31px" src="images/w3c_valid-xhtml10.png" alt="Valid XHTML 1.0!" width="88" height="31" />
+    </a>
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+      <img style="border:0;width:88px;height:31px" src="images/w3c_valid-css.png" alt="Valid CSS!" width="88" height="31" />
+    </a>
+  </div> <!-- icons -->
+</div>
+
+</div> <!-- /container -->
+
+</body>
+</html>
---
+++ tmpl/head.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+ <title>localstatic.com<?php if ("" != $page_title) echo " - " . $page_title; ?></title>
+ <link rel="stylesheet" type="text/css" href="../style.css"/>
+ <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+</head>
+<body>
+
+<div id="container">
+
+<div id="logo">
+<a href="/">localstatic.com</a>
+</div>
+
+<div id="nav">
+ <div class="nocss"><a href="#content">Jump to content</a></div>
+ <ul>
+  <li><a href=".">Home</a></li>
+  <li><a href="linux.php">Linux</a></li>
+  <li><a href="geocaching.php">Geocaching</a></li>
+  <li><a href="links.php">Links</a></li>
+  <li><a href="contact.php">Contact</a></li>
+  <li><a href="about.php">About</a></li>
+ </ul>
+</div>
+
+<div id="content">
+
