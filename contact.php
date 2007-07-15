ff4f1de36505f5a8426a84cc020e78a8e6a9dcee
mt <mt@25aa0472-a5d5-0310-b9f0-db4f2a0ab381>
2007-07-15 04:46:02 +0000

    git-svn-id: file:///home/mt/data/subversion/web/websites/localstatic.com@115 25aa0472-a5d5-0310-b9f0-db4f2a0ab381

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ $page_title = 'Contact Info';
 ?>
 
 <p>
-My email address is <a href="mailto:<?php echo $email_address; ?>"><?php echo $email_address; ?></a>.<br>
+My email address is <a href="mailto:localstatic@localstatic.com">localstatic@localstatic.com</a>.<br>
 My PGP key is:
 <?php
 include 'gpgkey.php';
