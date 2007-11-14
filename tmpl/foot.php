59edc904f77d1ca75d46e9a16d4c7fc89f152468
mt <mt@25aa0472-a5d5-0310-b9f0-db4f2a0ab381>
2007-11-14 06:57:12 +0000

    * Re-format code to be a little easier to read * Add "secondary" content div in foot.php to allow for miscellaneous info to be added to each page. Maybe social networking stuff, blogroll, etc.
    git-svn-id: file:///home/mt/data/subversion/web/websites/localstatic.com@147 25aa0472-a5d5-0310-b9f0-db4f2a0ab381

--- tmpl/foot.php
+++ tmpl/foot.php
@@ -1,24 +1,24 @@
 
-</div>
+	</div> <!-- /content -->
 
-<div id="footer">
-  <div id="meta">
-    <a href="mailto:localstatic@localstatic.com">Morgan Terry</a><br/>
-    <?php
-    //echo date('r', filemtime($include_file));
-    ?>
-  </div>
+	<div id="secondary">
 
-  <div id="icons">
-    <a href="http://validator.w3.org/check/referer">
-      <img style="border:0;width:88px;height:31px" src="images/w3c_valid-xhtml10.png" alt="Valid XHTML 1.0!" width="88" height="31" />
-    </a>
-    <a href="http://jigsaw.w3.org/css-validator/check/referer">
-      <img style="border:0;width:88px;height:31px" src="images/w3c_valid-css.png" alt="Valid CSS!" width="88" height="31" />
-    </a>
-  </div> <!-- icons -->
-</div>
+	</div>
 
+	<div id="footer">
+		<div id="meta">
+			<a href="mailto:localstatic@localstatic.com">Morgan Terry</a><br/>
+		</div>
+
+		<div id="icons">
+			<a href="http://validator.w3.org/check/referer">
+				<img style="border:0;width:88px;height:31px" src="images/w3c_valid-xhtml10.png" alt="Valid XHTML 1.0!" width="88" height="31" />
+			</a>
+			<a href="http://jigsaw.w3.org/css-validator/check/referer">
+				<img style="border:0;width:88px;height:31px" src="images/w3c_valid-css.png" alt="Valid CSS!" width="88" height="31" />
+			</a>
+		</div> <!-- icons -->
+	</div> <!-- footer -->
 </div> <!-- /container -->
 
 </body>
