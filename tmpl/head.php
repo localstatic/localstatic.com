e935267633f2e0dc3d33a86db71c63a2c7b2fd14
mt <mt@25aa0472-a5d5-0310-b9f0-db4f2a0ab381>
2008-11-24 06:32:28 +0000

    Added extra content to title area to show the page title after the "logo" link - e.g. "localstatic.com / Links" for the links page.
    git-svn-id: file:///home/mt/data/subversion/web/localstatic.com@198 25aa0472-a5d5-0310-b9f0-db4f2a0ab381

--- tmpl/head.php
+++ tmpl/head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<title>localstatic.com<?php if ("" != $page_title) echo " - " . $page_title; ?></title>
+	<title>localstatic.com<?php if ("" != $page_title) echo " - " . htmlentities($page_title); ?></title>
 	<link rel="stylesheet" type="text/css" href="style.css"/>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 </head>
@@ -10,7 +10,7 @@
 <div id="container">
 
 	<div id="logo">
-		<a href="/">localstatic.com</a>
+	<a href="/">localstatic.com</a><?php if ($page_title) echo ' / ' . htmlentities($page_title); ?>
 	</div>
 
 	<div id="nav">
