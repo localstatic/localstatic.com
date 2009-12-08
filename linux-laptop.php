8fff07def91bb09d40ada4fed0aa494eeb1ccbd7
mt <mt@25aa0472-a5d5-0310-b9f0-db4f2a0ab381>
2009-12-08 06:11:38 +0000

    Updated text to state that information is out-of-date (references old distributions)
    git-svn-id: file:///home/mt/data/subversion/web/localstatic.com@217 25aa0472-a5d5-0310-b9f0-db4f2a0ab381

--- linux-laptop.php
+++ linux-laptop.php
@@ -6,8 +6,7 @@ include 'tmpl/head.php';
 
 <h2>Linux on Toshiba Satellite 2100CDT</h2>
 
-<p>This page is where I will put any information that may be useful to someone setting up Linux on a laptop. I have a Toshiba Satellite 2100CDT that I got back in February 2000. I no longer use the machine, but for the whole time I did, it was running Linux. It still is, actually, when I do decide to boot it up. I still occasionally install a new distribution on it just for the hell of it, and it still works.</p>
-<p>Anyway, below you'll find documentation of some of my installs. I hope this helps someone. Please let me know if it does.</p>
+<p>These pages document my experiences with installing Linux on my old Toshiba Satellite 2100CDT that I got back in February 2000. I no longer have the machine, but for the whole time I did, it was running Linux. The information here is now out-of-date (old distributions, etc), but I'm leaving it up in hopes that it might still be useful to someone.</p>
 <p>In some of these pages, you'll notice that I've had problems getting PCMCIA to work properly. It worked fine with FreeBSD, and with the old stand-alone Linux PCMCIA drivers. It would not, however, work with the kernel PCMCIA drivers. I have since discovered, through experimentation and tips from readers of this site, that there is a BIOS option that you can change to get the Linux kernel PCMCIA stuff to work. Changing the "PC Card" setting to "Cardbus / 16-bit" solves the problem. The other two options ("PCIC Compatible" and "Auto") cause the system to lock up when trying to initialize the PCMCIA card services.</p>
 
 <p>
