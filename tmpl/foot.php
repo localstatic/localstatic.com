480e09ad0a9e3bd025bc0dfab840e3b9f53ae796
Morgan Terry <localstatic@localstatic.com>
2010-01-19 22:38:04 -0700

    Replaced W3C validation icons
    Replaced old yellow W3C icons with blue versions.

--- tmpl/foot.php
+++ tmpl/foot.php
@@ -12,10 +12,10 @@
 
 		<div id="icons">
 			<a href="http://validator.w3.org/check/referer">
-				<img style="border:0;width:88px;height:31px" src="images/w3c_valid-xhtml10.png" alt="Valid XHTML 1.0!" width="88" height="31" />
+				<img style="border:0;width:88px;height:31px" src="images/w3c_valid-xhtml10-blue.png" alt="Valid XHTML 1.0!" width="88" height="31" />
 			</a>
 			<a href="http://jigsaw.w3.org/css-validator/check/referer">
-				<img style="border:0;width:88px;height:31px" src="images/w3c_valid-css.png" alt="Valid CSS!" width="88" height="31" />
+				<img style="border:0;width:88px;height:31px" src="images/w3c_valid-css-blue.gif" alt="Valid CSS!" width="88" height="31" />
 			</a>
 		</div> <!-- /icons -->
 	</div> <!-- /footer -->
