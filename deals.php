62bb05c0315ff8329715bde7422e11c1e2b8e79a
Morgan Terry <localstatic@localstatic.com>
2011-02-12 20:18:16 -0700

    Use refer-a-friend link instead of affiliate link

--- deals.php
+++ deals.php
@@ -1,3 +1,4 @@
 <?php
-header('Location: http://www.citydeals.com/?ref=localstatic');
+//header('Location: http://www.citydeals.com/?ref=localstatic');
+header('Location: http://www.citydeals.com/?ref=CDUMTAwNjQ0');
 exit;
